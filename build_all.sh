4adaeb68599f9e12aca93e4ee090da0321397634
Chi Nul <chinul@disroot.org>
2018-07-25 13:42:40 +0000

    Add build_all.sh script

---
+++ build_all.sh
@@ -0,0 +1,26 @@
+cd common
+echo "Building common.."
+./gradlew generateproto
+./gradlew install
+echo
+
+cd ../p2p
+echo "Building p2p.."
+./gradlew install
+echo
+
+cd ../assets
+echo "Building assets.."
+./gradlew install
+echo
+
+cd ../core
+echo "Building core.."
+./gradlew install
+echo
+
+cd ../grpc
+echo "Building grpc.."
+./gradlew generateproto
+./gradlew install
+echo
