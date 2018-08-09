30686dccde3441b89ef2c07b489c299af1bf6cac
Chi Nul <chinul@disroot.org>
2018-08-09 14:37:00 +0000

    Update build command in build_all.sh comment

--- build_all.sh
+++ build_all.sh
@@ -5,7 +5,7 @@ echo "Building common.."
 echo
 
 # todo: try fat jar build if doing separate steps continue to have issues:
-#  ./gradlew --include-build ../common --include-build ../assets --include-build ../p2p --include-build ../core build -x test shadowJar
+#  ./gradlew --include-build ../common --include-build ../p2p --include-build ../core build -x test shadowJar
 
 cd ../p2p
 echo "Building p2p.."
