ec9de275cc46eb7082feadf4082ca8c6c1a2af2c
Chi Nul <chinul@disroot.org>
2018-08-11 14:19:18 +0200

    Use rebase instead of merge in fetch_all.sh

--- fetch_all.sh
+++ fetch_all.sh
@@ -5,42 +5,48 @@
 # todo: seednode should maybe be included
 set -eu
 echo "Fetching root.."
-git fetch manfred && git merge manfred/voting
+git fetch manfred
+git rebase manfred/voting
 git status
 git push
 echo
 
 cd common
 echo "Fetching common.."
-git fetch manfred && git merge manfred/voting
+git fetch manfred
+git rebase manfred/voting
 git status
 git push
 echo
 
 cd ../desktop
 echo "Fetching desktop.."
-git fetch manfred && git merge manfred/voting
+git fetch manfred
+git rebase manfred/voting
 git status
 git push
 echo
 
 cd ../p2p
 echo "Fetching p2p.."
-git fetch manfred && git merge manfred/voting
+git fetch manfred
+git rebase manfred/voting
 git status
 git push
 echo
 
 cd ../assets
 echo "Fetching assets.."
-git fetch manfred && git merge manfred/voting
+git fetch manfred
+git rebase manfred/voting
 git status
 git push
 echo
 
 cd ../core
 echo "Fetching core.."
-git fetch manfred && git merge manfred/voting
+git fetch manfred
+git rebase manfred/voting
 git status
 git push
 echo
