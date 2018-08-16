5e4c347b1ddf8aa5dc89336294ca58316694e7ec
Chi Nul <chinul@disroot.org>
2018-08-16 17:17:39 +0200

    Add start_arb.sh script

---
+++ start_arb.sh
@@ -0,0 +1,2 @@
+cd $HOME/src/github.com/chirhonul/bisq
+./desktop/build/app/bin/bisq-desktop --baseCurrencyNetwork=BTC_REGTEST --useLocalhost=true --myAddress=localhost:2222 --nodePort=2222 --appName=bisq-Local-Regtest-Arbitrator
